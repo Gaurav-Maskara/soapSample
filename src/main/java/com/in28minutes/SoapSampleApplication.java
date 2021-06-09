81dd383709e9ea2c63ed055957255b9c6c1dbc7e
Gaurav.Maskara <Gaurav.Maskara@IN2363675W0.ey.net>
2021-06-09 20:25:32 +0530

    changes

---
+++ src/main/java/com/in28minutes/SoapSampleApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapSampleApplication.class, args);
+	}
+
+}
